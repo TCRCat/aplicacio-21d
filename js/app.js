1aa848a27d189937650b290e4f50bee0e8192f70
Josep Sanz <josep.sanz@saltos.org>
2017-12-04 13:27:47 +0100

    Afegir els tabs al user interface

--- js/app.js
+++ js/app.js
@@ -1,52 +1,163 @@
 
-$("#user").val("foluwe");
-$("#pass").val("josepsanzcamp.123");
-
+// VARIABLES
 var user="";
 var pass="";
+
 var main="josepsanzcamp";
 var repo="test-21d";
+
 var sha1=""; // old object
 var sha2=""; // old tree
 var sha3=""; // new blob
 var sha4=""; // new tree
 var sha5=""; // new object
+
 var content="";
 var filename="";
 var message="";
 
-$("#button1").on("click",function() {
+var provincia="";
+var municipi="";
+var escola="";
+var mesa="";
+
+// EXTRES
+var candidatures={
+	"BARCELONA":{
+		"VPSC":1,
+		"VPP":1,
+		"VPACMA":1,
+		"VCS":1,
+		"VERC":1,
+		"VDIALEG":1,
+		"VCUP":1,
+		"VPUM":1,
+		"VCATCOMU":1,
+		"VJUNTS":1,
+		"VFAMILIA":1,
+		"VRECORTES":1,
+		"VDN":1,
+		"VPFIV":1,
+		"VCNV":1,
+		"VUNIDOS":1,
+		"VCILUS":0
+	},
+	"TARRAGONA":{
+		"VPSC":1,
+		"VPP":1,
+		"VPACMA":1,
+		"VCS":1,
+		"VERC":1,
+		"VDIALEG":1,
+		"VCUP":1,
+		"VPUM":1,
+		"VCATCOMU":1,
+		"VJUNTS":1,
+		"VFAMILIA":0,
+		"VRECORTES":1,
+		"VDN":0,
+		"VPFIV":0,
+		"VCNV":0,
+		"VUNIDOS":0,
+		"VCILUS":0
+	},
+	"LLEIDA":{
+		"VPSC":1,
+		"VPP":1,
+		"VPACMA":1,
+		"VCS":1,
+		"VERC":1,
+		"VDIALEG":1,
+		"VCUP":1,
+		"VPUM":1,
+		"VCATCOMU":1,
+		"VJUNTS":1,
+		"VFAMILIA":0,
+		"VRECORTES":1,
+		"VDN":0,
+		"VPFIV":0,
+		"VCNV":0,
+		"VUNIDOS":0,
+		"VCILUS":0
+	},
+	"GIRONA":{
+		"VPSC":1,
+		"VPP":1,
+		"VPACMA":1,
+		"VCS":1,
+		"VERC":1,
+		"VDIALEG":1,
+		"VCUP":1,
+		"VPUM":1,
+		"VCATCOMU":1,
+		"VJUNTS":1,
+		"VFAMILIA":0,
+		"VRECORTES":1,
+		"VDN":0,
+		"VPFIV":0,
+		"VCNV":0,
+		"VUNIDOS":0,
+		"VCILUS":1
+	}
+}
+
+// EVENTS
+$("#user,#pass").on("change",function() {
+	user=$("#user").val();
+	pass=$("#pass").val();
+	if(user!="" && pass!="") {
+		var hash=md5(user);
+		loading(true);
+		$.ajax({
+			url: "data/"+hash+".txt",
+			success: function(data,textStatus,jqXHR) {
+				loading(false);
+				$("#mesa").parent().removeClass("hidden");
+				data=data.split("\n");
+				$("#mesa option").remove();
+				for(var i in data) {
+					var val=data[i].split(";");
+					$("#mesa").append("<option value=\""+data[i]+"\">"+val[3]+"</option>");
+				}
+				$("#mesa").val($("#mesa option:first").val()).trigger("change");
+			},
+			error: function(jqXHR,textStatus,errorThrown){
+				loading(false);
+				$("#mesa option").remove();
+				$("#mesa").parent().addClass("hidden");
+				$("#mesa").trigger("change");
+			}
+		});
+	}
+});
+
+$("#mesa").on("change",function() {
+	var val=$("#mesa").val();
+	if(val) {
+		var val=val.split(";");
+		provincia=val[0];
+		municipi=val[1];
+		escola=val[2];
+		mesa=val[3];
+		$("#dades").parent().removeClass("hidden");
+		$("#dades").html(val.join("\n"));
+		$("#tabs").parent().removeClass("hidden");
+		maketab1(provincia);
+	} else {
+		$("#dades").parent().addClass("hidden");
+		$("#tabs").parent().addClass("hidden");
+	}
+})
+
+$("#enviar1").on("click",function() {
 	user=$("#user").val();
 	pass=$("#pass").val();
 	filename="resultats.txt";
-	message="Resultats: usuari "+user+", provincia B, municipi C, escola Y, mesa Z";
-	content="TODO";
-	makeall();
-});
-
-$("#button2").on("click",function() {
-	user=$("#user").val();
-	pass=$("#pass").val();
-	filename="acta.jpg";
-	message="Actes: usuari "+user+", provincia B, municipi C, escola Y, mesa Z";
-	var file=$("#camera").get(0).files[0];
-	var reader=new FileReader();
-	reader.onload=function(data) {
-		content=btoa(data.target.result);
-		makeall();
-	};
-	reader.onerror=function() {
-		console_log("error");
-	};
-	reader.readAsBinaryString(file);
-});
-
-$("#button3").on("click",function() {
-	user=$("#user").val();
-	pass=$("#pass").val();
-	filename="resultats.txt";
-	message="Incidencies: usuari "+user+", provincia B, municipi C, escola Y, mesa Z";
-	content=btoa($("#content").val());
+	message="Resultats: usuari "+user+", provincia "+provincia+", municipi "+municipi+", escola "+escola+", mesa "+mesa;
+	content=[];
+	var camps=["VPSC","VPP","VPACMA","VCS","VERC","VDIALEG","VCUP","VPUM","VCATCOMU","VJUNTS","VFAMILIA","VRECORTES","VDN","VPFIV","VCNV","VUNIDOS","VCILUS","VBLANC","VNULS","VTOTAL"];
+	for(var i in camps) content[i]=$("#"+camps[i]).val();
+	content=btoa(content.join(";"));
 	makeall();
 });
 
@@ -60,6 +171,33 @@ $("#camera2").on("click",function() {
 	$("#camera").trigger("click");
 });
 
+$("#enviar2").on("click",function() {
+	user=$("#user").val();
+	pass=$("#pass").val();
+	filename="acta.jpg";
+	message="Actes: usuari "+user+", provincia "+provincia+", municipi "+municipi+", escola "+escola+", mesa "+mesa;
+	var file=$("#camera").get(0).files[0];
+	var reader=new FileReader();
+	reader.onload=function(data) {
+		content=btoa(data.target.result);
+		makeall();
+	};
+	reader.onerror=function() {
+		console_log("error");
+	};
+	reader.readAsBinaryString(file);
+});
+
+$("#enviar3").on("click",function() {
+	user=$("#user").val();
+	pass=$("#pass").val();
+	filename="incidencia.txt";
+	message="Incidencies: usuari "+user+", provincia "+provincia+", municipi "+municipi+", escola "+escola+", mesa "+mesa;
+	content=btoa($("#content").val());
+	makeall();
+});
+
+// FUNCIONS
 function makeall() {
 	loading(true);
 	fork(function() {
@@ -67,8 +205,8 @@ function makeall() {
 		upload(function() {
 			console_log("upload done");
 			pullreq(function() {
-				console_log("pullreq done");
 				loading(false);
+				console_log("pullreq done");
 			});
 		});
 	});
@@ -85,8 +223,8 @@ function fork(fn) {
 			fn();
 		},
 		function(data) {
-			console_log(data);
 			loading(false);
+			console_log(data);
 		}
 	);
 }
@@ -165,38 +303,38 @@ function upload(fn) {
 													fn();
 												},
 												function(data) {
-													console_log(data);
 													loading(false);
+													console_log(data);
 												}
 											);
 										},
 										function(data) {
-											console_log(data);
 											loading(false);
+											console_log(data);
 										}
 									);
 								},
 								function(data) {
-									console_log(data);
 									loading(false);
+									console_log(data);
 								}
 							);
 						},
 						function(data) {
-							console_log(data);
 							loading(false);
+							console_log(data);
 						}
 					);
 				},
 				function(data) {
-					console_log(data);
 					loading(false);
+					console_log(data);
 				}
 			);
 		},
 		function(data) {
-			console_log(data);
 			loading(false);
+			console_log(data);
 		}
 	);
 }
@@ -216,8 +354,8 @@ function pullreq(fn) {
 			fn();
 		},
 		function(data) {
-			console_log(data);
 			loading(false);
+			console_log(data);
 		}
 	);
 }
@@ -243,10 +381,25 @@ function curl(user,pass,url,data,method,success,error) {
 }
 
 function console_log(text) {
+	$("#log").parent().removeClass("hidden");
 	$("#log").append(text+"\n");
 }
 
 function loading(bool) {
 	if(bool) $(".loading").show();
 	if(!bool) $(".loading").hide();
+}
+
+function maketab1(provincia) {
+	if(candidatures.hasOwnProperty(provincia)) {
+		for(var i in candidatures[provincia]) {
+			var key=i;
+			var val=candidatures[provincia][i];
+			if(val) {
+				$("#"+key).parent().removeClass("hidden");
+			} else {
+				$("#"+key).parent().addClass("hidden");
+			}
+		}
+	}
 }
